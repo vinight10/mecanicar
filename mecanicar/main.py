f51294bf75521b385e9de176a4c8d105a51f39cd
Vinicius Fiorezi <145400863+vinight10@users.noreply.github.com>
2024-02-23 06:22:56 +0000

    asdasd

--- mecanicar/main.py
+++ mecanicar/main.py
@@ -9,7 +9,7 @@ import time
 pd.set_option('display.max_rows', None)  # Exibir todas as linhas
 pd.set_option('display.max_columns', None)  # Exibir todas as colunas
 pd.set_option('display.width', None)  # Largura da tela (para evitar que as colunas sejam truncadas)
-pd.set_option('display.expand_frame_repr', False)  # Evitar que as colunas sejam truncadas
+pd.set_option('display.expand_frame_repr', True)  # Evitar que as colunas sejam truncadas
 pd.set_option('max_colwidth', None)  # Largura máxima da coluna (para evitar truncamento do conteúdo)
 
 conn = sqlite3.connect("database.db")
