d6b78b8a6a545030a08b8731c059ea262b734021
Vinicius Fiorezi <145400863+vinight10@users.noreply.github.com>
2024-02-23 06:19:41 +0000

    asdada

--- mecanicar/main.py
+++ mecanicar/main.py
@@ -115,7 +115,7 @@ elif choice == "Visualizar Todos os Veículos 📝":
                 success_message_text.text("Veículo deletado")
                 st.experimental_rerun()  # Rerun do script para atualizar em tempo real
         # Renderiza o DataFrame com a coluna de botões
-        st.dataframe(df_all.style.applymap(color_df, subset=["Status"]))
+        st.dataframe(df_all.style.applymap(color_df, subset=["Status"]),use_container_width=40)
     else:
         st.info("Nenhum veículo encontrado.")
 
